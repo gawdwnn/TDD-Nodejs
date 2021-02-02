23200bc7be676544186756172f70986446b1c1ed
smarttin <smarttin77@gmail.com>
2021-02-02 11:23:40 +0100

    changing page test

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import app from './src/app.js';
 import sequelize from './config/database.js';
+import User from './src/user/User.js';
 
-sequelize.sync({ force: true });
+const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+  for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
+    await User.create({
+      username: `user${i + 1}`,
+      email: `user${i + 1}@mail.com`,
+      inactive: i >= activeUserCount,
+    });
+  }
+};
+
+sequelize.sync({ force: true }).then(async () => {
+  await addUsers(25);
+});
 
 app.listen(3000, console.log('app is running'));
